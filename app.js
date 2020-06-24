7d2c60eff9923d9ef7ff8c3d4a1598ffea2f4fcc
Japan-ai <ai.1110.small.pond@gmail.com>
2020-06-24 19:41:18 +0900

    css-indexpage

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('hello.ejs');
-});
-
-app.get('/top',(req, res) => {
+app.get('/top', (req, res) => {
   res.render('top.ejs');
 });
 
-app.listen(3002);
+app.get('/index',(req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(3000);
